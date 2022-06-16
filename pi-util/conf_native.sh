22fe26e678703796e90ed8840837fb9127d2424c
John Cox <jc@kynesim.co.uk>
2022-06-16 10:59:15 +0000

    pi-util: conf: Add install dir params

--- pi-util/conf_native.sh
+++ pi-util/conf_native.sh
@@ -7,8 +7,10 @@ MC=`uname -m`
 if [ "$MC" == "armv7l" ]; then
 #  CONF_MMAL=--enable-mmal-avcodec
   CONF_MMAL=
+  A=arm-linux-gnueabihf
   ARM=armv7
 elif [ "$MC" == "aarch64" ]; then
+  A=aarch64-linux-gnu
   ARM=arm64
 else
   echo "Unkown machine name: $MC"
@@ -16,12 +18,22 @@ else
 fi
 OUT=$BASE/out/$ARM-`lsb_release -sc`-rel
 
+USR_PREFIX=$OUT/install
+LIB_PREFIX=$USR_PREFIX/lib/$A
+INC_PREFIX=$USR_PREFIX/include/$A
+
 echo "Configuring in $OUT"
 mkdir -p $OUT
 # Nothing under here need worry git - including this .gitignore!
 echo "**" > $BASE/out/.gitignore
 
 cd $OUT
-$BASE/configure  --disable-vdpau --enable-gles2 $CONF_MMAL
+$BASE/configure \
+ --prefix=$USR_PREFIX\
+ --libdir=$LIB_PREFIX\
+ --includedir=$INC_PREFIX\
+ --disable-vdpau\
+ --enable-gles2\
+ $CONF_MMAL
 echo "Configured in $OUT"
 
