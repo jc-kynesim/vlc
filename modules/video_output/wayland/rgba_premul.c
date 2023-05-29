40d852511e5f991b76b1c964b48cabf7281f2786
John Cox <jc@kynesim.co.uk>
2023-05-29 19:55:01 +0000

    Stash: fix unaligned & narrow frames

--- modules/video_output/wayland/rgba_premul.c
+++ modules/video_output/wayland/rgba_premul.c
@@ -15,6 +15,8 @@
 #endif
 
 // x, y src offset, not dest
+// This won't be bit exact with aarch64 asm which has slightly different
+// rounding (this is faster when done in C)
 static void
 copy_xxxa_with_premul_c(void * dst_data, int dst_stride,
                       const void * src_data, int src_stride,
@@ -88,6 +90,35 @@ utime(void)
     return ts.tv_nsec / 1000 + (uint64_t)ts.tv_sec * 1000000;
 }
 
+// What the ASM is meant to do exactly
+static void
+copy_xxxa_with_premul_c_asm(void * dst_data, int dst_stride,
+                      const void * src_data, int src_stride,
+                      const unsigned int w, const unsigned int h,
+                      const unsigned int global_alpha)
+{
+    uint8_t * dst = (uint8_t*)dst_data;
+    const uint8_t * src = (uint8_t*)src_data;
+    const int src_inc = src_stride - (int)w * 4;
+    const int dst_inc = dst_stride - (int)w * 4;
+
+    for (unsigned int i = 0; i != h; ++i)
+    {
+        for (unsigned int j = 0; j != w; ++j, src+=4, dst += 4)
+        {
+            // What the ASM is meant to do exactly
+            unsigned int a = global_alpha * 257;
+            const unsigned int k = 0x800000;
+            dst[0] = (((src[0] * src[3] * 257) >> 8) * a + k) >> 24;
+            dst[1] = (((src[1] * src[3] * 257) >> 8) * a + k) >> 24;
+            dst[2] = (((src[2] * src[3] * 257) >> 8) * a + k) >> 24;
+            dst[3] = (src[3] * global_alpha * 257 + 0x8000) >> 16;
+        }
+        src += src_inc;
+        dst += dst_inc;
+    }
+}
+
 static void
 timetest(const unsigned int w, const unsigned int h, const int s, bool use_c)
 {
@@ -114,6 +145,46 @@ timetest(const unsigned int w, const unsigned int h, const int s, bool use_c)
            (int)((done - now)/10));
 }
 
+static int
+docheck(const uint8_t * const a, const uint8_t * const b, const size_t n)
+{
+    int t = 0;
+    for (size_t i = 0; i != n && t < 128; ++i)
+    {
+        if (a[i] != b[i])
+        {
+            printf("@ %zd: %02x %02x\n", i, a[i], b[i]);
+            ++t;
+        }
+    }
+    return t;
+}
+
+static void
+checktest(const unsigned int w, const unsigned int h, const int s, const int offset)
+{
+    size_t dsize = ((h + 3) * s + 63) & ~63;
+
+    uint8_t * src = malloc(h * s);
+    uint8_t * dst = malloc(dsize);
+    uint8_t * dst2 = malloc(dsize);
+    uint8_t * d  = (uint8_t*)(((uintptr_t)dst + s + 63) & ~63);
+    uint8_t * d2 = (uint8_t*)(((uintptr_t)dst2 + s + 63) & ~63);
+
+    for (unsigned int i = 0; i != h * s; ++i)
+        src[i] = rand();
+
+    memset(dst2, 0xff, dsize);
+    memset(dst,  0xff, dsize);
+
+    copy_xxxa_with_premul_c_asm(d + offset, s, src, s, w, h, 0xba);
+    copy_xxxa_with_premul(d2 + offset, s, src, s, w, h, 0xba);
+
+    printf("Check: %dx%d stride %d offset %d: %s\n", w, h, s, offset,
+           docheck(d - s, d2 - s, (h + 2) * s) ? "FAIL" : "OK");
+}
+
+
 
 int
 main (int argc, char *argv[])
@@ -122,6 +193,23 @@ main (int argc, char *argv[])
     timetest(1920, 1080, 1920 * 4, false);
     timetest(1917, 1080, 1920 * 4, false);
     timetest(1917, 1080, 1917 * 4, false);
+    timetest(1920 * 1080, 1, 1920 * 1080 * 4, false);
+
+    checktest(1920, 1080, 1920 * 4, 0);
+
+    for (unsigned int i = 1; i != 64; ++i)
+    {
+        checktest(i, 16, 64 * 4, 0);
+    }
+    for (unsigned int i = 0; i != 16; ++i)
+    {
+        checktest(15, 16, 64 * 4, i * 4);
+        checktest(32, 16, 64 * 4, i * 4);
+        checktest(43, 16, 64 * 4, i * 4);
+    }
+
+
+
     return 0;
 }
 
