4e1d789365f4d4bd7ac8e8508af155655ad2f8f7
John Cox <jc@kynesim.co.uk>
2023-11-03 17:59:44 +0000

    pi-util/genpatch: Add qt wayland fullscreen patch

--- pi-util/genpatch.sh
+++ pi-util/genpatch.sh
@@ -62,6 +62,7 @@ git diff $REFNAME -- modules/video_chroma/chain.c > $DIFFBASE-002-chain.patch
 git diff $REFNAME -- bin/vlc.c > $DIFFBASE-003-vlc.patch
 git diff $REFNAME -- modules/video_output/caca.c > $DIFFBASE-004-caca.patch
 git diff $REFNAME -- modules/gui/qt/components/interface_widgets.* > $DIFFBASE-005-qt-wayland.patch
+git diff $REFNAME -- modules/gui/qt/components/controller.cpp > $DIFFBASE-006-qt-fullscreen.patch
 cd $DSTDIR
 zip -m $ZIPNAME $PATCHNAME-*.patch
 
