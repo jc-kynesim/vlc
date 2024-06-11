3f6ba466b7aa09a2b295619e37bcfa58f4f597c1
John Cox <jc@kynesim.co.uk>
2024-06-11 16:29:03 +0100

    vlc_fourcc: Add DRM_PRIME picture types
    (cherry picked from commit e59a64f0cb419ee80453b70a8281e216e4d5e3b4)

--- include/vlc_fourcc.h
+++ include/vlc_fourcc.h
@@ -494,6 +494,13 @@
 #define VLC_CODEC_NVDEC_OPAQUE_444      VLC_FOURCC('N','V','4','8') /* 4:4:4  8 bpc */
 #define VLC_CODEC_NVDEC_OPAQUE_444_16B  VLC_FOURCC('N','V','4','6') /* 4:4:4 16 bpc */
 
+/* DRM Prime */
+#define VLC_CODEC_DRM_PRIME_I420        VLC_FOURCC('D','P','V','0')
+#define VLC_CODEC_DRM_PRIME_NV12        VLC_FOURCC('D','P','N','1')
+#define VLC_CODEC_DRM_PRIME_SAND8       VLC_FOURCC('D','P','S','8')
+#define VLC_CODEC_DRM_PRIME_SAND30      VLC_FOURCC('D','P','S','3')
+#define VLC_CODEC_DRM_PRIME_RGB32       VLC_FOURCC('D','P','3','2')
+
 /* CVPixelBuffer opaque buffer type */
 #define VLC_CODEC_CVPX_NV12       VLC_FOURCC('C','V','P','N')
 #define VLC_CODEC_CVPX_UYVY       VLC_FOURCC('C','V','P','Y')
--- src/misc/fourcc.c
+++ src/misc/fourcc.c
@@ -423,6 +423,11 @@ static const vlc_chroma_description_t p_list_chroma_description[] = {
     { VLC_CODEC_VAAPI_444,             GPU_FMT(YUV444, 8) },
     { VLC_CODEC_VAAPI_444_10BPP,       GPU_FMT(YUV444, 10) },
     { VLC_CODEC_VAAPI_444_12BPP,       GPU_FMT(YUV444, 12) },
+
+    { VLC_CODEC_DRM_PRIME_I420,        GPU_FMT(YUV420, 8) },
+    { VLC_CODEC_DRM_PRIME_NV12,        GPU_FMT(YUV420, 8) },
+    { VLC_CODEC_DRM_PRIME_SAND8,       GPU_FMT(YUV420, 8) },
+    { VLC_CODEC_DRM_PRIME_SAND30,      GPU_FMT(YUV420, 10) },
 };
 
 #undef PACKED_FMT
