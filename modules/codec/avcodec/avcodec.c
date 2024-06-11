affd775261ebe4245c8ce7d05a5af31fef67fd31
John Cox <jc@kynesim.co.uk>
2024-06-11 16:25:24 +0100

    avcodec: Add AllocContextHw to try v4l2m2 decoders automatically

--- modules/codec/avcodec/avcodec.c
+++ modules/codec/avcodec/avcodec.c
@@ -221,17 +221,41 @@ vlc_module_begin ()
 #endif
 vlc_module_end ()
 
-AVCodecContext *ffmpeg_AllocContext( decoder_t *p_dec,
-                                     const AVCodec **restrict codecp )
+static const char *
+hw_v4l2m2m_dec_str(const unsigned i_codec_id)
+{
+    switch( i_codec_id )
+    {
+        case AV_CODEC_ID_MPEG2VIDEO:
+            return "mpeg2_v4l2m2m";
+        case AV_CODEC_ID_H264:
+            return "h264_v4l2m2m";
+        default:
+            break;
+    }
+    return NULL;
+}
+
+AVCodecContext *ffmpeg_AllocContextHw( decoder_t *p_dec,
+                                     const AVCodec **restrict codecp, const int hw )
 {
     enum AVCodecID i_codec_id;
     const char *psz_namecodec;
     const AVCodec *p_codec = NULL;
+    const char * hw_dec_name = NULL;
+    const char * const psz_decoder = var_InheritString( p_dec, "avcodec-codec" );
+
+    // If named decoder do not attempt hw override - wait for non-hw pass
+    if( hw != 0 && psz_decoder != NULL )
+        goto fail_free_psz_decoder;
 
     /* *** determine codec type *** */
     if( !GetFfmpegCodec( p_dec->fmt_in, &i_codec_id, &psz_namecodec ) ||
          i_codec_id == AV_CODEC_ID_RAWVIDEO )
-         return NULL;
+        goto fail_free_psz_decoder;
+
+    if( hw != 0 && (hw_dec_name = hw_v4l2m2m_dec_str(i_codec_id)) == NULL )
+        goto fail_free_psz_decoder;
 
     msg_Dbg( p_dec, "using %s %s", AVPROVIDER(LIBAVCODEC), LIBAVCODEC_IDENT );
 
@@ -239,7 +263,6 @@ AVCodecContext *ffmpeg_AllocContext( decoder_t *p_dec,
     vlc_init_avcodec(VLC_OBJECT(p_dec));
 
     /* *** ask ffmpeg for a decoder *** */
-    char *psz_decoder = var_InheritString( p_dec, "avcodec-codec" );
     if( psz_decoder != NULL )
     {
         p_codec = avcodec_find_decoder_by_name( psz_decoder );
@@ -251,9 +274,11 @@ AVCodecContext *ffmpeg_AllocContext( decoder_t *p_dec,
                     psz_decoder, (char*)&p_dec->fmt_in->i_codec );
             p_codec = NULL;
         }
-        free( psz_decoder );
+        free( (char*)psz_decoder );
     }
-    if( !p_codec )
+    if( hw_dec_name != NULL )
+        p_codec = avcodec_find_decoder_by_name(hw_dec_name);
+    else if( !p_codec )
         p_codec = avcodec_find_decoder( i_codec_id );
     if( !p_codec )
     {
@@ -271,6 +296,16 @@ AVCodecContext *ffmpeg_AllocContext( decoder_t *p_dec,
     avctx->debug = var_InheritInteger( p_dec, "avcodec-debug" );
     avctx->opaque = p_dec;
     return avctx;
+
+fail_free_psz_decoder:
+    free((char*)psz_decoder);
+    return NULL;
+}
+
+AVCodecContext *ffmpeg_AllocContext( decoder_t *p_dec,
+                                     const AVCodec **restrict codecp )
+{
+    return ffmpeg_AllocContextHw(p_dec, codecp, 0);
 }
 
 /*****************************************************************************
