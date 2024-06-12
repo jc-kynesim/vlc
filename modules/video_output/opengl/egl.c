3308a77ebcb8c0bde2432233fce1586e4983a978
John Cox <jc@kynesim.co.uk>
2024-06-12 10:16:27 +0100

    video_output: Prioritise GLES over GL
    Patched from VLC3 version
    (cherry picked from commit 55013be8894a827a310c2c228f6fcac7525ebfa9)

--- modules/video_output/opengl/egl.c
+++ modules/video_output/opengl/egl.c
@@ -896,7 +896,7 @@ vlc_module_begin ()
     add_shortcut ("egl")
 
     add_submodule ()
-    set_callback_opengl_es2(OpenGLES2, VLC_PRIORITY)
+    set_callback_opengl_es2(OpenGLES2, VLC_PRIORITY + 1)
     add_shortcut ("egl")
 
 vlc_module_end ()
