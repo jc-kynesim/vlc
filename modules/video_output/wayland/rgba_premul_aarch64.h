d933f880587c699a85e7580a31dcd7af010938e7
John Cox <jc@kynesim.co.uk>
2024-06-12 10:59:10 +0100

    video_output/wayland: Add dmabuf output
    Previously all output was via shm which can be inefficient.
    Depends on some files from drmu for DRM format info.
    
    (cherry picked from commit 75a8f3200eeb625767e7646ac0bb2be536375a9a)

---
+++ modules/video_output/wayland/rgba_premul_aarch64.h
@@ -0,0 +1,9 @@
+#ifndef _WAYLAND_RGBA_PREMUL_AARCH64_H
+#define _WAYLAND_RGBA_PREMUL_AARCH64_H
+
+void copy_xxxa_with_premul_aarch64(void * dst_data, int dst_stride,
+                      const void * src_data, int src_stride,
+                      const unsigned int w, const unsigned int h,
+                      const unsigned int global_alpha);
+
+#endif
