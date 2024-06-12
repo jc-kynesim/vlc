565022da7c394fa8e7d99210bfb57d58cf98ef72
John Cox <jc@kynesim.co.uk>
2024-06-12 10:48:19 +0100

    egl: Add avcodec DRM_PRIME decode path
    Also includes drm_prime based deinterlace and sand30 conversion
    functions.

---
+++ modules/hw/drm/drm_av_deinterlace.c
@@ -0,0 +1,335 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <vlc_common.h>
+#include <vlc_filter.h>
+#include <vlc_picture.h>
+#include <vlc_plugin.h>
+
+#include "../../codec/avcodec/drm_pic.h"
+
+#include <libavcodec/avcodec.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavformat/avformat.h>
+#include <libavutil/frame.h>
+#include <libavutil/opt.h>
+#include <libavutil/pixdesc.h>
+#include <libavutil/hwcontext.h>
+
+#define TRACE_ALL 0
+
+#define LIBAVUTIL_VERSION_CHECK( a, d, e ) \
+    (LIBAVUTIL_VERSION_MICRO >= 100 && LIBAVUTIL_VERSION_INT >= AV_VERSION_INT( a, d, e ) )
+
+typedef struct filter_sys_t {
+    AVFilterGraph *filter_graph;
+    AVFilterContext *buffersink_ctx;  // Allocated within graph - no explicit free
+    AVFilterContext *buffersrc_ctx;   // Allocated within graph - no explicit free
+    bool has_out;
+    AVFrame *out_frame;
+} filter_sys_t;
+
+static void drmp_av_flush(filter_t * filter)
+{
+    // Nothing to do
+    VLC_UNUSED(filter);
+
+#if TRACE_ALL
+    msg_Dbg(filter, "<<< %s", __func__);
+#endif
+}
+
+
+// Copied almost directly from ffmpeg filtering_video.c example
+static int init_filters(filter_t * const filter,
+                        const AVFrame * const frame)
+{
+    const char * const filters_descr = "deinterlace_v4l2m2m";
+    filter_sys_t *const sys = filter->p_sys;
+    int ret = 0;
+    const AVFilter *buffersrc  = avfilter_get_by_name("buffer");
+    const AVFilter *buffersink = avfilter_get_by_name("buffersink");
+    AVFilterInOut *outputs = avfilter_inout_alloc();
+    AVFilterInOut *inputs  = avfilter_inout_alloc();
+    static const enum AVPixelFormat pix_fmts[] = { AV_PIX_FMT_DRM_PRIME, AV_PIX_FMT_NONE };
+
+    msg_Dbg(filter, "Filter init start");
+
+    sys->out_frame = av_frame_alloc();
+    sys->filter_graph = avfilter_graph_alloc();
+    if (!outputs || !inputs || !sys->filter_graph || !sys->out_frame) {
+        ret = AVERROR(ENOMEM);
+        goto fail;
+    }
+
+    sys->buffersrc_ctx = avfilter_graph_alloc_filter(sys->filter_graph, buffersrc, "in");
+    if (sys->buffersrc_ctx == NULL) {
+        msg_Err(filter, "Cannot create buffer source");
+        goto fail;
+    }
+
+    {
+        AVBufferSrcParameters *par = av_buffersrc_parameters_alloc();
+        if (par == NULL) {
+            msg_Err(filter, "Failed to alloc buffersc parameters");
+            goto fail;
+        }
+        par->width =  frame->width;
+        par->height =  frame->height;
+        par->format = frame->format;
+#if LIBAVFILTER_BUILD >= AV_VERSION_INT(10, 1, 100)
+        par->color_range = frame->color_range;
+        par->color_space = frame->colorspace;
+#endif
+        par->time_base.num = 1;
+        par->time_base.den = CLOCK_FREQ;
+        par->sample_aspect_ratio = frame->sample_aspect_ratio;
+        // Buffersrc will take a ref - no need to keep locally
+        par->hw_frames_ctx = frame->hw_frames_ctx;
+        ret = av_buffersrc_parameters_set(sys->buffersrc_ctx, par);
+        av_freep(&par);
+        if (ret < 0) {
+            msg_Err(filter, "Failed to set buffersc parameters");
+            goto fail;
+        }
+    }
+
+    if (avfilter_init_dict(sys->buffersrc_ctx, NULL) < 0) {
+        msg_Err(filter, "Failed to init src dict");
+        goto fail;
+    }
+
+    /* buffer video sink: to terminate the filter chain. */
+    sys->buffersink_ctx = avfilter_graph_alloc_filter(sys->filter_graph, buffersink, "out");
+    if (sys->buffersink_ctx == NULL) {
+        msg_Err(filter, "Cannot create buffer sink");
+        goto fail;
+    }
+
+#if LIBAVFILTER_BUILD >= AV_VERSION_INT(10, 6, 100)
+    ret = av_opt_set_array(sys->buffersink_ctx, "pixel_formats",
+                           AV_OPT_SEARCH_CHILDREN | AV_OPT_ARRAY_REPLACE,
+                           0, sizeof(pix_fmts)/sizeof(pix_fmts[0]) - 1,
+                           AV_OPT_TYPE_PIXEL_FMT, pix_fmts);
+#else
+    ret = av_opt_set_int_list(sys->buffersink_ctx, "pix_fmts", pix_fmts,
+                              AV_PIX_FMT_NONE, AV_OPT_SEARCH_CHILDREN);
+#endif
+    if (ret < 0) {
+        msg_Err(filter, "Cannot set output pixel format");
+        goto fail;
+    }
+
+    if (avfilter_init_dict(sys->buffersink_ctx, NULL) < 0) {
+        msg_Err(filter, "Failed to init dest dict");
+        goto fail;
+    }
+
+    /*
+     * Set the endpoints for the filter graph. The filter_graph will
+     * be linked to the graph described by filters_descr.
+     */
+
+    /*
+     * The buffer source output must be connected to the input pad of
+     * the first filter described by filters_descr; since the first
+     * filter input label is not specified, it is set to "in" by
+     * default.
+     */
+    outputs->name       = av_strdup("in");
+    outputs->filter_ctx = sys->buffersrc_ctx;
+    outputs->pad_idx    = 0;
+    outputs->next       = NULL;
+
+    /*
+     * The buffer sink input must be connected to the output pad of
+     * the last filter described by filters_descr; since the last
+     * filter output label is not specified, it is set to "out" by
+     * default.
+     */
+    inputs->name       = av_strdup("out");
+    inputs->filter_ctx = sys->buffersink_ctx;
+    inputs->pad_idx    = 0;
+    inputs->next       = NULL;
+
+    if ((ret = avfilter_graph_parse_ptr(sys->filter_graph, filters_descr,
+                                    &inputs, &outputs, NULL)) < 0) {
+        msg_Err(filter, "Filter graph parse failed");
+        goto fail;
+    }
+
+    if ((ret = avfilter_graph_config(sys->filter_graph, NULL)) < 0) {
+        msg_Err(filter, "Filter graph config failed");
+        goto fail;
+    }
+
+    msg_Dbg(filter, "Filter init OK");
+    avfilter_inout_free(&inputs);
+    avfilter_inout_free(&outputs);
+    return VLC_SUCCESS;
+
+fail:
+    msg_Dbg(filter, "Filter init FAIL");
+    avfilter_inout_free(&inputs);
+    avfilter_inout_free(&outputs);
+    av_frame_free(&sys->out_frame);
+    avfilter_graph_free(&sys->filter_graph);
+    sys->buffersink_ctx = NULL;
+    sys->buffersrc_ctx = NULL;
+    return ret == AVERROR(ENOMEM) ? VLC_ENOMEM : VLC_EGENERIC;
+}
+
+static picture_t * drmp_av_deinterlace(filter_t * filter, picture_t * in_pic)
+{
+    filter_sys_t *const sys = filter->p_sys;
+    AVFrame * frame = av_frame_alloc();
+    drm_prime_video_sys_t * const pctx = (drm_prime_video_sys_t *)in_pic->context;
+    picture_t * out_pic = NULL;
+    picture_t ** pp_pic = &out_pic;
+    int ret;
+
+#if TRACE_ALL
+    msg_Dbg(filter, "<<< %s", __func__);
+#endif
+
+    if (!frame) {
+        msg_Err(filter, "Frame alloc failure");
+        goto fail;
+    }
+
+    frame->format      = AV_PIX_FMT_DRM_PRIME;
+    frame->buf[0]      = av_buffer_ref(pctx->buf);
+    frame->data[0]     = (uint8_t *)pctx->desc;
+    frame->hw_frames_ctx = av_buffer_ref(pctx->hw_frames_ctx);
+    frame->width       = in_pic->format.i_width;
+    frame->height      = in_pic->format.i_height;
+    frame->crop_left   = in_pic->format.i_x_offset;
+    frame->crop_top    = in_pic->format.i_y_offset;
+    frame->crop_right  = frame->width -  in_pic->format.i_visible_width -  frame->crop_left;
+    frame->crop_bottom = frame->height - in_pic->format.i_visible_height - frame->crop_top;
+#if LIBAVUTIL_VERSION_CHECK( 58, 7, 100 )
+    frame->flags       = 0;
+    if (!in_pic->b_progressive)
+        frame->flags |= AV_FRAME_FLAG_INTERLACED;
+    if (in_pic->b_top_field_first)
+        frame->flags |= AV_FRAME_FLAG_TOP_FIELD_FIRST;
+#else
+    frame->interlaced_frame = !in_pic->b_progressive;
+    frame->top_field_first  = in_pic->b_top_field_first;
+#endif
+    frame->pts         = (in_pic->date == VLC_TS_INVALID) ? AV_NOPTS_VALUE : in_pic->date;
+
+    picture_Release(in_pic);
+    in_pic = NULL;
+
+    if (!sys->filter_graph) {
+        if (init_filters(filter, frame) != 0) {
+            msg_Err(filter, "Filter init failure");
+            goto fail;
+        }
+    }
+
+    if ((ret = av_buffersrc_add_frame_flags(sys->buffersrc_ctx, frame, AV_BUFFERSRC_FLAG_KEEP_REF)) < 0) {
+        msg_Err(filter, "Failed to feed filtergraph: %s", av_err2str(ret));
+        goto fail;
+    }
+    av_frame_unref(frame);
+
+    while (sys->has_out || (ret = av_buffersink_get_frame(sys->buffersink_ctx, sys->out_frame)) == 0) {
+        picture_t *const pic = filter_NewPicture(filter);
+        sys->has_out = true;
+        // Failure to get an output pic happens quite often, just keep the
+        // frame for next time
+        if (!pic)
+            break;
+
+        if (drm_prime_attach_buf_to_pic(pic, sys->out_frame) != VLC_SUCCESS) {
+            msg_Err(filter, "Failed to attach frame to out pic");
+            picture_Release(pic);
+            goto fail;
+        }
+        pic->date =  sys->out_frame->pts == AV_NOPTS_VALUE ? VLC_TS_INVALID : sys->out_frame->pts;
+        av_frame_unref(sys->out_frame);
+        sys->has_out = false;
+
+        *pp_pic = pic;
+        pp_pic = &pic->p_next;
+    }
+
+    if (ret < 0 && ret != AVERROR_EOF && ret != AVERROR(EAGAIN)) {
+        msg_Err(filter, "Failed to get frame: %s", av_err2str(ret));
+        goto fail;
+    }
+
+    // Even if we get an error we may have processed some pics and we need to return them
+fail:
+    if (in_pic)
+        picture_Release(in_pic);
+    av_frame_free(&frame);
+
+#if TRACE_ALL
+    msg_Dbg(filter, ">>> %s: %p", __func__, out_pic);
+#endif
+
+    return out_pic;
+}
+
+static void CloseDrmpAvDeinterlace(filter_t *filter)
+{
+    filter_sys_t * const sys = filter->p_sys;
+
+#if TRACE_ALL
+    msg_Dbg(filter, "<<< %s", __func__);
+#endif
+
+    if (sys == NULL)
+        return;
+
+    av_frame_free(&sys->out_frame);
+    avfilter_graph_free(&sys->filter_graph);
+    free(sys);
+}
+
+static bool is_fmt_valid_in(const vlc_fourcc_t fmt)
+{
+    return fmt == VLC_CODEC_DRM_PRIME_I420 ||
+            fmt == VLC_CODEC_DRM_PRIME_NV12 ||
+            fmt == VLC_CODEC_DRM_PRIME_SAND8;
+}
+
+static int OpenDrmpAvDeinterlace(filter_t *filter)
+{
+    filter_sys_t *sys;
+
+    msg_Dbg(filter, "<<< %s", __func__);
+
+    if (!is_fmt_valid_in(filter->fmt_in.video.i_chroma) ||
+        filter->fmt_out.video.i_chroma != filter->fmt_in.video.i_chroma)
+        return VLC_EGENERIC;
+
+    sys = calloc(1, sizeof(filter_sys_t));
+    if (!sys)
+        return VLC_ENOMEM;
+    filter->p_sys = sys;
+
+    // We would like to init the filter stack here but we need hw_frames_ctx
+    // and we don't have that till we have the first frame :-(
+
+    filter->pf_video_filter = drmp_av_deinterlace;
+    filter->pf_flush = drmp_av_flush;
+
+    return VLC_SUCCESS;
+}
+
+vlc_module_begin()
+    set_shortname(N_("DRM PRIME deinterlace"))
+    set_description(N_("libav-based DRM_PRIME deinterlace filter plugin"))
+    set_capability("video filter", 902)
+    set_category(CAT_VIDEO)
+    set_subcategory(SUBCAT_VIDEO_VFILTER)
+    set_callbacks(OpenDrmpAvDeinterlace, CloseDrmpAvDeinterlace)
+    add_shortcut("deinterlace")
+vlc_module_end()
+
---
+++ modules/hw/drm/drm_avcodec.c
@@ -0,0 +1,80 @@
+/*****************************************************************************
+ * avcodec.c: VDPAU decoder for libav
+ *****************************************************************************
+ * Copyright (C) 2012-2013 Rémi Denis-Courmont
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <string.h>
+#include <stdlib.h>
+
+#include <vlc_common.h>
+#include <vlc_plugin.h>
+#include <vlc_fourcc.h>
+#include <vlc_picture.h>
+#include "../../codec/avcodec/va.h"
+
+// Dummy - not expected to be called
+static int drm_va_get(vlc_va_t *va, picture_t *pic, uint8_t **data)
+{
+    VLC_UNUSED(va);
+    VLC_UNUSED(pic);
+    VLC_UNUSED(data);
+
+    return VLC_SUCCESS;
+}
+
+static int Open(vlc_va_t *va, AVCodecContext *avctx, const AVPixFmtDescriptor *desc,
+                enum PixelFormat pix_fmt,
+                const es_format_t *fmt, picture_sys_t *p_sys)
+{
+    VLC_UNUSED(fmt);
+    VLC_UNUSED(p_sys);
+    VLC_UNUSED(desc);
+
+    msg_Dbg(va, "%s: pix_fmt=%d", __func__, pix_fmt);
+
+    if (pix_fmt != AV_PIX_FMT_DRM_PRIME)
+        return VLC_EGENERIC;
+
+    // This gives us whatever the decode requires + 6 frames that will be
+    // alloced by ffmpeg before it blocks (at least for Pi HEVC)
+    avctx->extra_hw_frames = 6;
+
+    va->description = "DRM Video Accel";
+    va->get = drm_va_get;
+    return VLC_SUCCESS;
+}
+
+static void Close(vlc_va_t *va, void **hwctx)
+{
+    VLC_UNUSED(hwctx);
+
+    msg_Dbg(va, "%s", __func__);
+}
+
+vlc_module_begin()
+    set_description(N_("DRM video decoder"))
+    set_capability("hw decoder", 100)
+    set_category(CAT_INPUT)
+    set_subcategory(SUBCAT_INPUT_VCODEC)
+    set_callbacks(Open, Close)
+    add_shortcut("drm_prime")
+vlc_module_end()
