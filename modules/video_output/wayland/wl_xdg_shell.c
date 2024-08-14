50be13360438f649fb7ed8bcba73def5bb509115
John Cox <jc@kynesim.co.uk>
2024-08-14 11:57:45 +0100

    Stash: Start mouse support

--- modules/video_output/wayland/wl_xdg_shell.c
+++ modules/video_output/wayland/wl_xdg_shell.c
@@ -48,6 +48,9 @@ struct vout_window_sys_t
     struct org_kde_kwin_server_decoration_manager *deco_manager;
     struct org_kde_kwin_server_decoration *deco;
 
+    struct wl_seat *wl_seat;
+    struct wl_pointer *wl_pointer;
+
     bool req_fullscreen;
     unsigned int req_width;
     unsigned int req_height;
@@ -265,6 +268,454 @@ static const struct xdg_wm_base_listener xdg_shell_cbs =
     xdg_shell_ping_cb,
 };
 
+// ----------------------------------------------------------------------------
+//
+// Mouse
+
+static void
+pointer_destroy(struct wl_pointer **ppPointer)
+{
+    struct wl_pointer * const p = *ppPointer;
+    if (p == NULL)
+        return;
+    *ppPointer = NULL;
+    wl_pointer_destroy(p);
+}
+
+/**
+ * enter event
+ *
+ * Notification that this seat's pointer is focused on a certain
+ * surface.
+ *
+ * When a seat's focus enters a surface, the pointer image is
+ * undefined and a client should respond to this event by setting
+ * an appropriate pointer image with the set_cursor request.
+ * @param serial serial number of the enter event
+ * @param surface surface entered by the pointer
+ * @param surface_x surface-local x coordinate
+ * @param surface_y surface-local y coordinate
+ */
+static void pointer_enter_cb(void *data,
+          struct wl_pointer *wl_pointer,
+          uint32_t serial,
+          struct wl_surface *surface,
+          wl_fixed_t surface_x,
+          wl_fixed_t surface_y)
+{
+}
+
+/**
+ * leave event
+ *
+ * Notification that this seat's pointer is no longer focused on
+ * a certain surface.
+ *
+ * The leave notification is sent before the enter notification for
+ * the new focus.
+ * @param serial serial number of the leave event
+ * @param surface surface left by the pointer
+ */
+static void pointer_leave_cb(void *data,
+          struct wl_pointer *wl_pointer,
+          uint32_t serial,
+          struct wl_surface *surface)
+{
+}
+
+/**
+ * pointer motion event
+ *
+ * Notification of pointer location change. The arguments
+ * surface_x and surface_y are the location relative to the focused
+ * surface.
+ * @param time timestamp with millisecond granularity
+ * @param surface_x surface-local x coordinate
+ * @param surface_y surface-local y coordinate
+ */
+static void pointer_motion_cb(void *data,
+           struct wl_pointer *wl_pointer,
+           uint32_t time,
+           wl_fixed_t surface_x,
+           wl_fixed_t surface_y)
+{
+}
+
+/**
+ * pointer button event
+ *
+ * Mouse button click and release notifications.
+ *
+ * The location of the click is given by the last motion or enter
+ * event. The time argument is a timestamp with millisecond
+ * granularity, with an undefined base.
+ *
+ * The button is a button code as defined in the Linux kernel's
+ * linux/input-event-codes.h header file, e.g. BTN_LEFT.
+ *
+ * Any 16-bit button code value is reserved for future additions to
+ * the kernel's event code list. All other button codes above
+ * 0xFFFF are currently undefined but may be used in future
+ * versions of this protocol.
+ * @param serial serial number of the button event
+ * @param time timestamp with millisecond granularity
+ * @param button button that produced the event
+ * @param state physical state of the button
+ */
+static void 
+pointer_button_cb(void *data,
+           struct wl_pointer *wl_pointer,
+           uint32_t serial,
+           uint32_t time,
+           uint32_t button,
+           uint32_t state)
+{
+}
+
+/**
+ * axis event
+ *
+ * Scroll and other axis notifications.
+ *
+ * For scroll events (vertical and horizontal scroll axes), the
+ * value parameter is the length of a vector along the specified
+ * axis in a coordinate space identical to those of motion events,
+ * representing a relative movement along the specified axis.
+ *
+ * For devices that support movements non-parallel to axes multiple
+ * axis events will be emitted.
+ *
+ * When applicable, for example for touch pads, the server can
+ * choose to emit scroll events where the motion vector is
+ * equivalent to a motion event vector.
+ *
+ * When applicable, a client can transform its content relative to
+ * the scroll distance.
+ * @param time timestamp with millisecond granularity
+ * @param axis axis type
+ * @param value length of vector in surface-local coordinate space
+ */
+static void pointer_axis_cb(void *data,
+         struct wl_pointer *wl_pointer,
+         uint32_t time,
+         uint32_t axis,
+         wl_fixed_t value)
+{
+    VLC_UNUSED(data);
+    VLC_UNUSED(wl_pointer);
+    VLC_UNUSED(time);
+    VLC_UNUSED(axis);
+    VLC_UNUSED(value);
+}
+
+/**
+ * end of a pointer event sequence
+ *
+ * Indicates the end of a set of events that logically belong
+ * together. A client is expected to accumulate the data in all
+ * events within the frame before proceeding.
+ *
+ * All wl_pointer events before a wl_pointer.frame event belong
+ * logically together. For example, in a diagonal scroll motion the
+ * compositor will send an optional wl_pointer.axis_source event,
+ * two wl_pointer.axis events (horizontal and vertical) and finally
+ * a wl_pointer.frame event. The client may use this information to
+ * calculate a diagonal vector for scrolling.
+ *
+ * When multiple wl_pointer.axis events occur within the same
+ * frame, the motion vector is the combined motion of all events.
+ * When a wl_pointer.axis and a wl_pointer.axis_stop event occur
+ * within the same frame, this indicates that axis movement in one
+ * axis has stopped but continues in the other axis. When multiple
+ * wl_pointer.axis_stop events occur within the same frame, this
+ * indicates that these axes stopped in the same instance.
+ *
+ * A wl_pointer.frame event is sent for every logical event group,
+ * even if the group only contains a single wl_pointer event.
+ * Specifically, a client may get a sequence: motion, frame,
+ * button, frame, axis, frame, axis_stop, frame.
+ *
+ * The wl_pointer.enter and wl_pointer.leave events are logical
+ * events generated by the compositor and not the hardware. These
+ * events are also grouped by a wl_pointer.frame. When a pointer
+ * moves from one surface to another, a compositor should group the
+ * wl_pointer.leave event within the same wl_pointer.frame.
+ * However, a client must not rely on wl_pointer.leave and
+ * wl_pointer.enter being in the same wl_pointer.frame.
+ * Compositor-specific policies may require the wl_pointer.leave
+ * and wl_pointer.enter event being split across multiple
+ * wl_pointer.frame groups.
+ * @since 5
+ */
+static void pointer_frame_cb(void *data,
+          struct wl_pointer *wl_pointer)
+{
+}
+
+/**
+ * axis source event
+ *
+ * Source information for scroll and other axes.
+ *
+ * This event does not occur on its own. It is sent before a
+ * wl_pointer.frame event and carries the source information for
+ * all events within that frame.
+ *
+ * The source specifies how this event was generated. If the source
+ * is wl_pointer.axis_source.finger, a wl_pointer.axis_stop event
+ * will be sent when the user lifts the finger off the device.
+ *
+ * If the source is wl_pointer.axis_source.wheel,
+ * wl_pointer.axis_source.wheel_tilt or
+ * wl_pointer.axis_source.continuous, a wl_pointer.axis_stop event
+ * may or may not be sent. Whether a compositor sends an axis_stop
+ * event for these sources is hardware-specific and
+ * implementation-dependent; clients must not rely on receiving an
+ * axis_stop event for these scroll sources and should treat scroll
+ * sequences from these scroll sources as unterminated by default.
+ *
+ * This event is optional. If the source is unknown for a
+ * particular axis event sequence, no event is sent. Only one
+ * wl_pointer.axis_source event is permitted per frame.
+ *
+ * The order of wl_pointer.axis_discrete and wl_pointer.axis_source
+ * is not guaranteed.
+ * @param axis_source source of the axis event
+ * @since 5
+ */
+static void
+pointer_axis_source_cb(void *data,
+            struct wl_pointer *wl_pointer,
+            uint32_t axis_source)
+{
+    VLC_UNUSED(data);
+    VLC_UNUSED(wl_pointer);
+    VLC_UNUSED(axis_source);
+}
+
+/**
+ * axis stop event
+ *
+ * Stop notification for scroll and other axes.
+ *
+ * For some wl_pointer.axis_source types, a wl_pointer.axis_stop
+ * event is sent to notify a client that the axis sequence has
+ * terminated. This enables the client to implement kinetic
+ * scrolling. See the wl_pointer.axis_source documentation for
+ * information on when this event may be generated.
+ *
+ * Any wl_pointer.axis events with the same axis_source after this
+ * event should be considered as the start of a new axis motion.
+ *
+ * The timestamp is to be interpreted identical to the timestamp in
+ * the wl_pointer.axis event. The timestamp value may be the same
+ * as a preceding wl_pointer.axis event.
+ * @param time timestamp with millisecond granularity
+ * @param axis the axis stopped with this event
+ * @since 5
+ */
+static void
+pointer_axis_stop_cb(void *data,
+          struct wl_pointer *wl_pointer,
+          uint32_t time,
+          uint32_t axis)
+{
+    VLC_UNUSED(data);
+    VLC_UNUSED(wl_pointer);
+    VLC_UNUSED(time);
+    VLC_UNUSED(axis);
+}
+
+/**
+ * axis click event
+ *
+ * Discrete step information for scroll and other axes.
+ *
+ * This event carries the axis value of the wl_pointer.axis event
+ * in discrete steps (e.g. mouse wheel clicks).
+ *
+ * This event is deprecated with wl_pointer version 8 - this event
+ * is not sent to clients supporting version 8 or later.
+ *
+ * This event does not occur on its own, it is coupled with a
+ * wl_pointer.axis event that represents this axis value on a
+ * continuous scale. The protocol guarantees that each
+ * axis_discrete event is always followed by exactly one axis event
+ * with the same axis number within the same wl_pointer.frame. Note
+ * that the protocol allows for other events to occur between the
+ * axis_discrete and its coupled axis event, including other
+ * axis_discrete or axis events. A wl_pointer.frame must not
+ * contain more than one axis_discrete event per axis type.
+ *
+ * This event is optional; continuous scrolling devices like
+ * two-finger scrolling on touchpads do not have discrete steps and
+ * do not generate this event.
+ *
+ * The discrete value carries the directional information. e.g. a
+ * value of -2 is two steps towards the negative direction of this
+ * axis.
+ *
+ * The axis number is identical to the axis number in the
+ * associated axis event.
+ *
+ * The order of wl_pointer.axis_discrete and wl_pointer.axis_source
+ * is not guaranteed.
+ * @param axis axis type
+ * @param discrete number of steps
+ * @since 5
+ */
+static void
+pointer_axis_discrete_cb(void *data,
+              struct wl_pointer *wl_pointer,
+              uint32_t axis,
+              int32_t discrete)
+{
+    VLC_UNUSED(data);
+    VLC_UNUSED(wl_pointer);
+    VLC_UNUSED(axis);
+    VLC_UNUSED(discrete);
+}
+
+/**
+ * axis high-resolution scroll event
+ *
+ * Discrete high-resolution scroll information.
+ *
+ * This event carries high-resolution wheel scroll information,
+ * with each multiple of 120 representing one logical scroll step
+ * (a wheel detent). For example, an axis_value120 of 30 is one
+ * quarter of a logical scroll step in the positive direction, a
+ * value120 of -240 are two logical scroll steps in the negative
+ * direction within the same hardware event. Clients that rely on
+ * discrete scrolling should accumulate the value120 to multiples
+ * of 120 before processing the event.
+ *
+ * The value120 must not be zero.
+ *
+ * This event replaces the wl_pointer.axis_discrete event in
+ * clients supporting wl_pointer version 8 or later.
+ *
+ * Where a wl_pointer.axis_source event occurs in the same
+ * wl_pointer.frame, the axis source applies to this event.
+ *
+ * The order of wl_pointer.axis_value120 and wl_pointer.axis_source
+ * is not guaranteed.
+ * @param axis axis type
+ * @param value120 scroll distance as fraction of 120
+ * @since 8
+ */
+static void pointer_axis_value120_cb(void *data,
+              struct wl_pointer *wl_pointer,
+              uint32_t axis,
+              int32_t value120)
+{
+    VLC_UNUSED(data);
+    VLC_UNUSED(wl_pointer);
+    VLC_UNUSED(axis);
+    VLC_UNUSED(value120);
+}
+
+/**
+ * axis relative physical direction event
+ *
+ * Relative directional information of the entity causing the
+ * axis motion.
+ *
+ * For a wl_pointer.axis event, the
+ * wl_pointer.axis_relative_direction event specifies the movement
+ * direction of the entity causing the wl_pointer.axis event. For
+ * example: - if a user's fingers on a touchpad move down and this
+ * causes a wl_pointer.axis vertical_scroll down event, the
+ * physical direction is 'identical' - if a user's fingers on a
+ * touchpad move down and this causes a wl_pointer.axis
+ * vertical_scroll up scroll up event ('natural scrolling'), the
+ * physical direction is 'inverted'.
+ *
+ * A client may use this information to adjust scroll motion of
+ * components. Specifically, enabling natural scrolling causes the
+ * content to change direction compared to traditional scrolling.
+ * Some widgets like volume control sliders should usually match
+ * the physical direction regardless of whether natural scrolling
+ * is active. This event enables clients to match the scroll
+ * direction of a widget to the physical direction.
+ *
+ * This event does not occur on its own, it is coupled with a
+ * wl_pointer.axis event that represents this axis value. The
+ * protocol guarantees that each axis_relative_direction event is
+ * always followed by exactly one axis event with the same axis
+ * number within the same wl_pointer.frame. Note that the protocol
+ * allows for other events to occur between the
+ * axis_relative_direction and its coupled axis event.
+ *
+ * The axis number is identical to the axis number in the
+ * associated axis event.
+ *
+ * The order of wl_pointer.axis_relative_direction,
+ * wl_pointer.axis_discrete and wl_pointer.axis_source is not
+ * guaranteed.
+ * @param axis axis type
+ * @param direction physical direction relative to axis motion
+ * @since 9
+ */
+static void pointer_axis_relative_direction_cb(void *data,
+                struct wl_pointer *wl_pointer,
+                uint32_t axis,
+                uint32_t direction)
+{
+    VLC_UNUSED(data);
+    VLC_UNUSED(wl_pointer);
+    VLC_UNUSED(axis);
+    VLC_UNUSED(direction);
+}
+
+static const struct wl_pointer_listener pointer_cbs = {
+	.enter = pointer_enter_cb,
+	.leave = pointer_leave_cb,
+	.motion = pointer_motion_cb,
+	.button = pointer_button_cb,
+	.axis = pointer_axis_cb,
+	.frame = pointer_frame_cb,
+	.axis_source = pointer_axis_source_cb,
+	.axis_stop = pointer_axis_stop_cb,
+	.axis_discrete = pointer_axis_discrete_cb,
+	.axis_value120 = pointer_axis_value120_cb,
+	.axis_relative_direction = pointer_axis_relative_direction_cb,
+};
+
+static void
+seat_capabilities_cb(void *data, struct wl_seat *wl_seat, uint32_t capabilities)
+{
+    vout_window_t * const wnd = data;
+    vout_window_sys_t * const sys = wnd->sys;
+    VLC_UNUSED(wl_seat);
+    msg_Dbg(wnd, "%s: Caps: %#"PRIx32, __func__, capabilities);
+
+    if ((capabilities & WL_SEAT_CAPABILITY_POINTER) != 0)
+    {
+        sys->wl_pointer = wl_seat_get_pointer(wl_seat);
+        wl_pointer_add_listener(sys->wl_pointer, &pointer_cbs, wnd);
+    }
+    else
+    {
+        pointer_destroy(&sys->wl_pointer);
+    }
+}
+
+static void
+seat_name_cb(void *data, struct wl_seat *wl_seat, const char *name)
+{
+    vout_window_t * const wnd = data;
+    VLC_UNUSED(wl_seat);
+    msg_Dbg(wnd, "%s: %s", __func__, name);
+}
+
+static const struct wl_seat_listener seat_cbs = {
+    .capabilities = seat_capabilities_cb,
+    .name = seat_name_cb
+};
+
+// ----------------------------------------------------------------------------
+
 static void registry_global_cb(void *data, struct wl_registry *registry,
                                uint32_t name, const char *iface, uint32_t vers)
 {
@@ -280,7 +731,11 @@ static void registry_global_cb(void *data, struct wl_registry *registry,
     else
     if (!strcmp(iface, xdg_wm_base_interface.name))
         sys->shell = wl_registry_bind(registry, name, &xdg_wm_base_interface, 1);
-    else
+    if (!strcmp(iface, wl_seat_interface.name))
+    {
+        sys->wl_seat = wl_registry_bind(registry, name, &wl_seat_interface, 2);
+        wl_seat_add_listener(sys->wl_seat, &seat_cbs, wnd);
+    } else
     if (!strcmp(iface, "org_kde_kwin_server_decoration_manager"))
         sys->deco_manager = wl_registry_bind(registry, name,
                          &org_kde_kwin_server_decoration_manager_interface, 1);
@@ -422,6 +877,8 @@ error:
         xdg_surface_destroy(sys->xdg_surface);
     if (sys->shell != NULL)
         xdg_wm_base_destroy(sys->shell);
+    if (sys->wl_seat)
+        wl_seat_destroy(sys->wl_seat);
     if (sys->compositor != NULL)
         wl_compositor_destroy(sys->compositor);
     wl_display_disconnect(display);
@@ -447,6 +904,8 @@ static void Close(vout_window_t *wnd)
     xdg_surface_destroy(sys->xdg_surface);
     wl_surface_destroy(wnd->handle.wl);
     xdg_wm_base_destroy(sys->shell);
+    pointer_destroy(&sys->wl_pointer);
+    wl_seat_destroy(sys->wl_seat);
     wl_compositor_destroy(sys->compositor);
     wl_display_disconnect(wnd->display.wl);
     free(sys);
