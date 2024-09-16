e7bc8b1cdcc6d82a2d2eb828b041f727e82f3a59
John Cox <jc@kynesim.co.uk>
2024-09-16 11:52:38 +0100

    drmu: Update pollqueue

--- modules/video_output/drmu/pollqueue.c
+++ modules/video_output/drmu/pollqueue.c
@@ -60,9 +60,6 @@ struct pollqueue {
         void *v;
     } prepost;
 
-    void (* exit_fn)(void * v);
-    void * exit_v;
-
     bool kill;
     bool join_req;  // On thread exit do not detach
     bool no_prod;
@@ -165,10 +162,14 @@ static void polltask_dead(struct polltask * const pt)
     pthread_cond_broadcast(&pt->q->cond);
 }
 
-static int pollqueue_prod(const struct pollqueue *const pq)
+static void pollqueue_prod(const struct pollqueue *const pq)
 {
     static const uint64_t one = 1;
-    return write(pq->prod_fd, &one, sizeof(one));
+    int rv;
+    while ((rv = write(pq->prod_fd, &one, sizeof(one))) != sizeof(one)) {
+        if (!(rv == -1 && errno == EINTR))
+            break;
+    }
 }
 
 void polltask_delete(struct polltask **const ppt)
@@ -277,13 +278,10 @@ static void *poll_thread(void *v)
         struct polltask *pt;
         struct polltask *pt_next;
         struct prepost_ss prepost;
-        uint64_t now = pollqueue_now(0);
-        int timeout = -1;
+        uint64_t timeout0 = 0;
         int rv;
 
         for (pt = pq->head; pt; pt = pt_next) {
-            int64_t t;
-
             pt_next = pt->next;
 
             if (pt->state == POLLTASK_Q_KILL) {
@@ -305,10 +303,11 @@ static void *poll_thread(void *v)
                 };
             }
 
-            t = (int64_t)(pt->timeout - now);
-            if (pt->timeout && t < INT_MAX &&
-                (timeout < 0 || (int)t < timeout))
-                timeout = (t < 0) ? 0 : (int)t;
+            // Get earliest timeout
+            if (pt->timeout != 0 &&
+                (timeout0 == 0 || (int64_t)(pt->timeout - timeout0) < 0))
+                timeout0 = pt->timeout;
+
             ++nall;
         }
         prepost = pq->prepost;
@@ -318,11 +317,14 @@ static void *poll_thread(void *v)
         if (prepost.pre)
             prepost.pre(prepost.v, a + npoll);
 
-        while ((rv = poll(a, npoll + (a[npoll].fd != -1), timeout)) == -1)
-        {
-            if (errno != EINTR)
-                break;
-        }
+        do {
+            const int64_t diff = (int64_t)(timeout0 - pollqueue_now(0));
+            const int timeout = timeout0 == 0 ? -1 :
+                                diff <= 0 ? 0 :
+                                diff >= INT_MAX ? INT_MAX : (int)diff;
+
+            rv = poll(a, npoll + (a[npoll].fd != -1), timeout);
+        } while (rv == -1 && errno == EINTR);
 
         if (prepost.post)
             prepost.post(prepost.v, a[npoll].revents);
@@ -331,8 +333,6 @@ static void *poll_thread(void *v)
             request_log("Poll error: %s\n", strerror(errno));
             goto fail_unlocked;
         }
-
-        now = pollqueue_now(0);
 
         pthread_mutex_lock(&pq->lock);
         /* Prodding in this loop is pointless and might lead to
@@ -354,8 +354,12 @@ static void *poll_thread(void *v)
             if (pt->state != POLLTASK_QUEUED)
                 continue;
 
-            /* Pending? */
-            if (r || (pt->timeout && (int64_t)(now - pt->timeout) >= 0)) {
+            /* Pending?
+             * Take time as intended time rather than actual time.
+             * probably makes no actual difference and saves us a call
+             */
+            if (r || (pt->timeout != 0 && timeout0 != 0 &&
+                      (int64_t)(timeout0 - pt->timeout) >= 0)) {
                 pollqueue_rem_task(pq, pt);
                 pt->state = POLLTASK_RUNNING;
                 pthread_mutex_unlock(&pq->lock);
@@ -383,21 +387,13 @@ static void *poll_thread(void *v)
     pthread_mutex_unlock(&pq->lock);
 fail_unlocked:
 
-    {
-        void (*const exit_fn)(void *v) = pq->exit_fn;
-        void * const exit_v = pq->exit_v;
-
-        polltask_free(pq->prod_pt);
-        pthread_cond_destroy(&pq->cond);
-        pthread_mutex_destroy(&pq->lock);
-        close(pq->prod_fd);
-        if (!pq->join_req)
-            pthread_detach(pthread_self());
-        free(pq);
-
-        if (exit_fn)
-            exit_fn(exit_v);
-    }
+    polltask_free(pq->prod_pt);
+    pthread_cond_destroy(&pq->cond);
+    pthread_mutex_destroy(&pq->lock);
+    close(pq->prod_fd);
+    if (!pq->join_req)
+        pthread_detach(pthread_self());
+    free(pq);
 
     return NULL;
 }
@@ -406,8 +402,13 @@ static void prod_fn(void *v, short revents)
 {
     struct pollqueue *const pq = v;
     char buf[8];
-    if (revents)
-        read(pq->prod_fd, buf, 8);
+    if (revents) {
+        int rv;
+        while ((rv = read(pq->prod_fd, buf, 8)) != 8) {
+            if (!(rv == -1 && errno == EINTR))
+                break;
+        }
+    }
     if (!pq->kill)
         pollqueue_add_task(pq->prod_pt, -1);
 }
@@ -534,12 +535,5 @@ void pollqueue_set_pre_post(struct pollqueue *const pq,
             rv = pthread_cond_wait(&pq->cond, &pq->lock);
     }
     pthread_mutex_unlock(&pq->lock);
-}
-
-void pollqueue_set_exit(struct pollqueue *const pq,
-                        void (* const exit_fn)(void * v), void * v)
-{
-    pq->exit_fn = exit_fn;
-    pq->exit_v = v;
 }
 
