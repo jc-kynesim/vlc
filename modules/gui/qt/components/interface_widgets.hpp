85edcc0451b76143aeafe74e8725f848dc6c39c7
John Cox <jc@kynesim.co.uk>
2026-01-08 19:54:08 +0000

    qt: Fix compile of interface widget patch

--- modules/gui/qt/components/interface_widgets.hpp
+++ modules/gui/qt/components/interface_widgets.hpp
@@ -76,7 +76,6 @@ protected:
 #endif
     void hideEvent(QHideEvent *event) Q_DECL_OVERRIDE;
     void showEvent(QShowEvent *event) Q_DECL_OVERRIDE;
-    bool nativeEvent(const QByteArray &eventType, void *message, long *result) Q_DECL_OVERRIDE;
     virtual void resizeEvent(QResizeEvent *) Q_DECL_OVERRIDE;
     void mousePressEvent(QMouseEvent *) Q_DECL_OVERRIDE;
     void mouseMoveEvent(QMouseEvent *) Q_DECL_OVERRIDE;
